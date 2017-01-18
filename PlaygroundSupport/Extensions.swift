1fa181e24fd14ab34a81fe526e0782fa275e37c7
Marcus R. Brown <me@igetgam.es>
2017-01-18 01:34:47 -0700

    Add an exension to confirm UIViewController to the PlaygorundRemoteLiveView protocol.

---
+++ PlaygroundSupport/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  PlaygroundSupport
+//
+//  Created by  on 1/18/17.
+//  Copyright © 2017 Lou Franco. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController: PlaygroundRemoteLiveView {
+    public func send(_ value: PlaygroundValue) {
+        // Stub
+    }
+}
